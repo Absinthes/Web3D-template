782de3630059be0c805dab9e7dbf7e17288ab43a
Absinthe <1198727859@qq.com>
2024-02-26 10:25:35 +0800

    feat: 遗漏的makeDefault

--- src/views/Camera.tsx
+++ src/views/Camera.tsx
@@ -1,15 +1,9 @@
 import { PerspectiveCamera } from "@react-three/drei";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { Camera as CameraImpl } from "three";
-
-export let globalCamera: CameraImpl;
 
 export default function Camera() {
   const cameraRef = useRef<CameraImpl>(null);
 
-  useEffect(() => {
-    globalCamera = cameraRef.current!;
-  });
-
-  return <PerspectiveCamera ref={cameraRef} />;
+  return <PerspectiveCamera makeDefault ref={cameraRef} />;
 }
--- src/views/Controls.tsx
+++ src/views/Controls.tsx
@@ -1,19 +1,13 @@
 import { OrbitControls } from "@react-three/drei";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { OrbitControls as OrbitControlsImpl } from "three-stdlib";
-
-export let globalOrbitControls: OrbitControlsImpl;
 
 export default function Controls() {
   const orbitControlsRef = useRef<OrbitControlsImpl>(null);
 
-  useEffect(() => {
-    globalOrbitControls = orbitControlsRef.current!;
-  });
-
   return (
     <>
-      <OrbitControls ref={orbitControlsRef} />
+      <OrbitControls makeDefault ref={orbitControlsRef} />
     </>
   );
 }
